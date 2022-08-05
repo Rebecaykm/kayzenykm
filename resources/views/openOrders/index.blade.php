1d024b034609652096ef197236e3a6d4ca882e14
Jesus Adrian Camacho Rocha <jesus.camacho@ykm.com.mx>
2022-08-05 11:54:18 -0500

    [Edit] Loading Spinner

--- resources/views/openOrders/index.blade.php
+++ resources/views/openOrders/index.blade.php
@@ -14,7 +14,9 @@
         <form method="get" action="{{ route('open-orders.index') }}">
             <div class="flex flex-row grid grid-cols-4 items-center my-2">
                 <div class="col-span-2">
-
+                    <h2 class="my-2 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+                        Open shop order report
+                    </h2>
                 </div>
                 <div class="col-span-2 flex flex-row justify-end">
                     <input type="date" id="due_date" name="due_date"
@@ -34,26 +36,25 @@
         </form>
 
         <div class="w-full overflow-hidden rounded-lg shadow-xs px-4 py-3 mb-8 bg-white shadow-md dark:bg-gray-800">
-            <div class="w-full overflow-x-auto">
-                <form method="post" action="{{ route('open-orders.store') }}">
-                    @csrf
-                    <div class="m-2 flex flex-row items-center justify-between">
-                        <h4 class="mb-4 text-xl font-bold text-gray-600 dark:text-gray-300">
-                            Open Shop Order Report
-                        </h4>
-                        <div class="flex flex-end">
-                            <span class="flex col-span-6 m-2 sm:m-auto sm:justify-end sm:text-xs">
-                                {{ $openOrders->withQueryString()->links() }}
-                            </span>
-                            <button type="submit"
-                                class="flex items-center justify-end p-2 m-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue">
-                                <span class="mx-2">Update</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                  </svg>
-                            </button>
-                        </div>
+            <form method="post" action="{{ route('open-orders.store') }}">
+                @csrf
+                <div class="flex flex-row items-center justify-end">
+                    <div class="flex flex-end">
+                        {{-- <span class="flex m-2 sm:m-auto sm:justify-end sm:text-xs">
+                            {{ $openOrders->withQueryString()->links() }}
+                        </span> --}}
+                        <button type="submit"
+                            class="flex items-center justify-end p-2 m-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-blue-600 hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue">
+                            <span class="mx-2">Update</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none"viewBox="0 0 24 24"
+                                stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                            </svg>
+                        </button>
                     </div>
+                </div>
+                <div class="w-full overflow-x-auto">
                     <table class="w-full whitespace-no-wrap">
                         <thead>
                             <tr
@@ -132,8 +133,8 @@
                             @endforeach
                         </tbody>
                     </table>
-                </form>
-            </div>
+                </div>
+            </form>
             <div
                 class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
                 <span class="flex items-center col-span-3">
