be73479f8da36ff649b408f5f04a9e325d6a0752
Adrian Camacho <jesus.camacho@ykm.com.mx>
2022-11-11 08:25:02 -0600

    excel2

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -11,27 +11,11 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 
 class UsersExport implements FromView
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function headings(): array
-    {
-        return [
-            'final',
-            'componente',
-            'clase',
-            'Activo'
-        ];
-    }
-    public function collection()
-    {
-         $mostar = Structure::Query()
-         ->select( 'final', 'componente','clase', 'Activo')
-         ->orderby('componente');
-         return $mostar;
 
-    }
     public function view(): view
     {
+        return view('exports.invoices', [
+            'invoices' => Invoice::all()
+        ]);
     }
 }
