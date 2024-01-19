69d4a1c1ddd19479ba8e6b1b76939d5c6b308eef
Jesus Adrian Camacho Rocha <jesus.camacho@ykm.com.mx>
2024-01-19 17:15:50 -0600

    ya no aguanto los ojos

--- resources/views/treeNode.blade.php
+++ resources/views/treeNode.blade.php
@@ -1,12 +1,15 @@
 <!-- resources/views/partials/treeNode.blade.php -->
 
-<li>
-    {{ $node['number'] }}
+<li class="mb-2 tree-node">
+    <div>
+        <span>{{ $node['number'] }}</span>
+        <div class="tree-line"></div>
+    </div>
     @if (!empty($node['subParts']))
-    <ul>
-        @foreach ($node['subParts'] as $subNode)
-        @include('treeNode', ['node' => $subNode])
-        @endforeach
-    </ul>
+        <ul class="list-none ml-4">
+            @foreach ($node['subParts'] as $subNode)
+                @include('treeNode', ['node' => $subNode])
+            @endforeach
+        </ul>
     @endif
 </li>
