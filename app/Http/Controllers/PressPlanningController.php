d337f3ea778c4439446698f32539ceea215a7be7
Miguel Alcala <miguel.alcala@ykm.com.mx>
2024-02-05 15:19:10 -0600

    vista final plan

--- app/Http/Controllers/PressPlanningController.php
+++ app/Http/Controllers/PressPlanningController.php
@@ -31,6 +31,7 @@ class PressPlanningController extends Controller
     {
 
         $numberparts = PartNumber::where('workcenter_id', $request->SePress)->get();
+
         $numberpartsArray = PartNumber::where('workcenter_id', $request->SePress)->get()->toarray();
         $PartNumberId = array_column($numberpartsArray, 'number');
         $hoy = $request->fecha;
@@ -40,135 +41,178 @@ class PressPlanningController extends Controller
 
 
         $rel = [];
+        $bomin = [];
+        $mat = [];
         foreach ($numberparts  as $fsopart) {
             //    dd( $fsopart,$fsopart->subPartNumbers);
 
-            foreach (array_column($fsopart->subPartNumbers->toArray(), 'number') as $num) {
-                array_push($rel, $num);
+            $bomnum = [];
+            $bomin = [];
+            $arr = array_column($fsopart->subPartNumbers->toArray(), 'number');
+            $reversed = array_reverse($arr);
+
+            if (!in_array($reversed[0],   $mat)) {
+                array_push($mat, $reversed[0]);
+                $bomin += ['Mat' => $reversed[0]];
+                unset($reversed[0]);
+                array_push($bomnum, $reversed);
+                $bomin += ['parts' => $reversed];
+                array_push($rel, $bomin);
+            } else {
+
+                $pos = array_search($reversed[0], $mat);
+                unset($reversed[0]);
+                $var = $rel[$pos];
+                foreach ($reversed as $p2) {
+                    if (!array_search($p2, $var['parts'])) {
+                        array_push($var['parts'], $p2);
+                    }
+                }
+                $rel[$pos] =  $var;
             }
         }
+        $part = [];
+        foreach (array_column($rel, 'parts') as $parts) {
+            foreach ($parts as $pq) {
+                array_push($part, $pq);
+            }
+        }
+
+
 
         $valPDp = FSO::query()
             ->select('SPROD', 'SDDTE',  'SOCNO', 'SQREQ', 'SQFIN')
-            ->wherein('SPROD', $rel)
+            ->wherein('SPROD', $part)
             ->where('SDDTE', '>=', $hoy)
             ->where('SDDTE', '<', $totalF)
             ->get()->toarray();
+            $ifso = Array_column($valPDp, 'SPROD');
 
         $ultfso = FSO::query()
-            ->select('SPROD',FSO::raw('MAX(SDDTE) AS SDDTE1 '),FSO::raw(' max(SOCNO) AS SOCNO'), FSO::raw('MAX(SQREQ) AS SREQ'), FSO::raw('MAX(SQFIN) AS REAL'))
-            ->wherein('SPROD', $rel)
-            ->where('SDDTE','<=',$totalF)
-            ->ORDERBY('SDDTE1','DESC')
+            ->select('SPROD', FSO::raw('MAX(SDDTE) AS SDDTE1 '), FSO::raw(' max(SOCNO) AS SOCNO'), FSO::raw('MAX(SQREQ) AS SREQ'), FSO::raw('MAX(SQFIN) AS REAL'))
+            ->wherein('SPROD', $part)
+            ->where('SDDTE', '<=', $totalF)
+            ->ORDERBY('SDDTE1', 'DESC')
             ->groupBy('SPROD')
             ->get()->toarray();
 
-            $itemfso=Array_column($ultfso,'SPROD');
-            $datfso=Array_column($ultfso,'SDDTE1');
-            $nofso=Array_column($ultfso,'SOCNO');
-            $reqfso=Array_column($ultfso,'SREQ');
-            $finfso=Array_column($ultfso,'REAL');
+        $itemfso = Array_column($ultfso, 'SPROD');
+
         $valHPO = HPO::query()
             ->select('PORD', 'PLINE', 'PPROD', 'PQORD', 'PQREC', 'PDDTE')
-            ->wherein('PPROD', $rel)
+            ->wherein('PPROD', array_column($rel, 'Mat'))
             ->where('PDDTE', '>=', $hoy)
             ->where('PDDTE', '<', $totalF)
             ->get()->toarray();
-        // $valHPO = HPO::query()
-        //     ->select('PORD', 'PLINE', 'PPROD', 'PQORD', 'PQREC', 'PDDTE')
-        //     ->wherein('PPROD', $rel)
-        //     ->where('PDDTE', '<', $hoy)
+        $itemHPO = Array_column($valHPO, 'PPROD');
 
-        //     ->get()->toarray();
-        $data = [];
-
-
-
-        foreach ($rel as $key => $num) {
-
-            $info = [];
+        $ultHPo = HPO::query()
+            ->select('PPROD', HPO::raw('MAX(PDDTE) AS PDDTE1 '), HPO::raw(' max(PORD) AS PORD'), HPO::raw('MAX(PQORD) AS PQREC'), HPO::raw('MAX(PQREC) AS REAL'), HPO::raw('MAX(PLINE) AS PLINE'))
+            ->wherein('PPROD', array_column($rel, 'Mat'))
+            ->where('PDDTE', '<=', $totalF)
+            ->ORDERBY('PDDTE1', 'DESC')
+            ->groupBy('PPROD')
+            ->get()->toarray();
+        $itemultHPO = Array_column($ultHPo, 'PPROD');
 
 
-            $pos=array_search($num, $itemfso);
-            $info["prod"]=$itemfso[$pos];
-            $info["fecha"] = $datfso[$pos];
-            $info["R"]=$reqfso[$pos];
-            $info["F"]=$finfso[$pos];
+        $bom = [];
 
 
+        foreach ($rel as $arre) {
+            $data = [];
 
+            $datos = [];
+            $fechaval = [];
+            $pos = array_search($arre['Mat'],   $itemultHPO);
+            $parts = [];
+            $parts += ["mat" => $arre['Mat']];
+            if ($pos !== false) {
+                $datos = $ultHPo[$pos];
+                $fechaval += ['Fecha' => $datos['PDDTE1']];
+                $fechaval += ['R' => $datos['PQREC']];
+                $fechaval += ['F' => $datos['REAL']];
+                $fechaval += ['orden' => $datos['PORD']];
+                $fechaval += ['LINEA' => $datos['PLINE']];
+                $parts += ["ultimaorden" => $fechaval];
 
-
-            if (strstr($num, "-MAT") == false) {
-                $info = [];
-                $info["parte"] = $num;
-                $pos=array_search($num, $itemfso);
-                $info["prod"]=$itemfso[$pos];
-                $info["fecha"] = $datfso[$pos];
-                $info["R"]=$reqfso[$pos];
-                $info["F"]=$finfso[$pos];
-
-                $day_qty = [];
-
-
-                foreach ($valPDp as $key => $fsopartnum) {
-                    $test1 = $fsopartnum['SPROD'];
-                    if ($num == $test1) {
-
-                        $day_qty += [$fsopartnum['SDDTE'] . 'R' => $fsopartnum['SQREQ']];
-                        $day_qty += [$fsopartnum['SDDTE'] . 'F' => $fsopartnum['SQFIN']];
-                    }
-                }
-                $info["produccion"] = $day_qty;
-                array_push($data, $info);
+            }else
+            {$fechaval += ['Fecha' => ''];
+                $fechaval += ['R' => ''];
+                $fechaval += ['F' => ''];
+                $fechaval += ['orden' =>''];
+                $fechaval += ['LINEA' => ''];
+                $parts += ["ultimaorden" => $fechaval];
             }
 
 
-            if (strstr($num, "-MAT") !== false) {
-                $info = [];
-                $info["parte"] = $num;
-                $info["parte"] = $num;
-                $pos=array_search($num, $itemfso);
-                $info["prod"]=$itemfso[$pos];
-                $info["fecha"] = $datfso[$pos];
-                $info["R"]=$reqfso[$pos];
-                $info["F"]=$finfso[$pos];
-                $day_hpo = [];
-                foreach ($valHPO as $key => $hpopartnum) {
-                    if ($num == $hpopartnum['PPROD']) {
-                        $day_hpo += [$hpopartnum['PDDTE'] . 'R' => $hpopartnum['PQORD']];
-                        $day_hpo += [$hpopartnum['PDDTE'] . 'F' => $hpopartnum['PQREC']];
-                        // dd('hola puto ');
-                    }
-                }
-                // para fines de vista el  se pone produccion
-                $info["produccion"] = $day_hpo;
-
-                array_push($data, $info);
+            $totalsemana=[];
+            $semana=[];
+            while($vl=array_search($arre['Mat'],  $itemHPO)!==false)
+            {
+                $semana=[];
+                // echo  $arre['Mat'].'<br>';
+                $pos=array_search($arre['Mat'],  $itemHPO);
+                $valarre= $valHPO[$pos];
+                $semana += [ 'R' =>   $valarre['PQORD']];
+                $semana += [  'F' =>  $valarre['PQREC']];
+                $semana+= ['orden' =>   $valarre['PORD']];
+                $semana += ['LINEA' =>   $valarre['PLINE']];
+                unset($itemHPO[$pos]);
+               $totalsemana+=[$valarre['PDDTE']=>$semana];
             }
+
+            $semana=[];
+
+            $parts += ["semana" =>$totalsemana];
+            $data += ["Mat" => $parts];
+
+            $itemHPO = Array_column($valHPO, 'PPROD');
+            $fechaval = [];
+            $hijo = [];
+            foreach ($arre['parts'] as $part1) {
+                $parts = [];
+                $pos1 = array_search($part1,   $itemfso);
+                $dat = $ultfso[$pos1];
+                $parts += ['parte' => $part1];
+                $fechaval = ['Fecha' => $dat['SDDTE1']];
+                $fechaval += ["R" => $dat['SREQ']];
+                $fechaval += ["F" => $dat['REAL']];
+                $fechaval += ["TURNO" => $dat['SOCNO']];
+                $parts += ['ultimaorden' => $fechaval];
+
+
+                $totalsemana=[];
+
+                while($vl1=array_search($part1,    $ifso )!==false)
+                {
+
+                    $semana=[];
+                    // echo  $part1.'<br>';
+                    $pos=array_search($part1, $ifso);
+                    $valarre= $valPDp[$pos];
+                    $semana += [  'R' =>   $valarre['SQREQ']];
+                    $semana += [  'F' =>  $valarre['SQFIN']];
+                    unset( $ifso[$pos]);
+                    $totalsemana+=[$valarre['SDDTE']=>$semana];
+                }
+                $parts += ["semana" =>$totalsemana];
+                array_push($hijo,  $parts);
+                $ifso = Array_column($valPDp, 'SPROD');
+
+            }
+
+            $data += ["hijo" =>  $hijo];
+            array_push($bom, $data);
+            $ifso = Array_column($valPDp, 'SPROD');
         }
 
 
-        $bus = array_column($data, 'parte');
-
-        $total = [];
-        foreach ($numberparts  as $fsopart) {
-            $arr_num = [];
-
-            foreach (array_column($fsopart->subPartNumbers->toArray(), 'number') as $num) {
 
 
-                $lug = array_search($num, $bus);
 
 
-                array_push($arr_num, $data[$lug]);
-            }
-
-            array_push($total, $arr_num);
-        }
-
-        dd($total);
-        return view('pressplanning.plancomponente', ['res' => $total,   'fecha' => $request->fecha, 'dias' => $dias]);
+        return view('pressplanning.plancomponente', ['res' => $bom,   'fecha' => $request->fecha, 'dias' => $dias]);
     }
     /**
      * Show the form for creating a new resource.
