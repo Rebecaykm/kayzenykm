a1277cd4c3a21f5329eb6d1c87afdcdd7a055da2
Jesus Adrian Camacho Rocha <jesus.camacho@ykm.com.mx>
2024-02-13 17:17:46 -0600

    fix: corrección en sql para editar scrap

--- resources/views/scrap-record/index.blade.php
+++ resources/views/scrap-record/index.blade.php
@@ -36,19 +36,19 @@
                 <table class="w-full whitespace-no-wrap table-fixed">
                     <thead>
                         <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-                            <th class="px-4 py-3">{{ __('Departamento') }}</th>
+                            <th class="px-4 py-3">{{ __('Estación') }}</th>
                             <th class="px-4 py-3">{{ __('Número de Parte') }}</th>
                             <th class="px-4 py-3">{{ __('Scrap') }}</th>
                             <th class="px-4 py-3">{{ __('Cantidad') }}</th>
                             <th class="px-4 py-3">{{ __('Fecha') }}</th>
-                            <!-- <th class="px-4 py-3">{{ __('Acciones') }}</th> -->
+                            <th class="px-4 py-3">{{ __('Acciones') }}</th>
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y dark:divide-gray-700 uppercase dark:bg-gray-800">
                         @foreach ($scrapRecords as $scrapRecord)
                         <tr class="text-gray-700 dark:text-gray-400">
                             <td class="px-4 py-3 text-xs">
-                                {{ $scrapRecord->partNumber->workcenter->departament->name ?? '' }}
+                                {{ $scrapRecord->partNumber->workcenter->number ?? '' }} - {{ $scrapRecord->partNumber->workcenter->name ?? '' }}
                             </td>
                             <td class="px-4 py-3 text-xs">
                                 {{ $scrapRecord->partNumber->number ?? '' }}
@@ -57,12 +57,12 @@
                                 {{ $scrapRecord->scrap->code ?? '' }} - {{ $scrapRecord->scrap->name ?? '' }}
                             </td>
                             <td class="px-4 py-3 text-sm">
-                                {{ $scrapRecord->quantity_scrap ?? '' }}
+                                {{ intval($scrapRecord->quantity_scrap) ?? '' }}
                             </td>
                             <td class="px-4 py-3 text-sm">
                                 {{ $scrapRecord->created_at->format('d-m-Y H:i:s') ?? '' }}
                             </td>
-                            <!-- <td class="px-4 py-3">
+                            <td class="px-4 py-3">
                                 <div class="flex items-center space-x-4 text-sm">
                                     <a href="{{ route('scrap-record.edit', $scrapRecord->id) }}" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit">
                                         <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
@@ -70,17 +70,9 @@
                                             </path>
                                         </svg>
                                     </a>
-                                    <form action="{{ route('scrap-record.destroy', $scrapRecord->id) }}" method="post">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
-                                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-                                            </svg>
-                                        </button>
-                                    </form>
+
                                 </div>
-                            </td> -->
+                            </td>
                         </tr>
                         @endforeach
                     </tbody>
