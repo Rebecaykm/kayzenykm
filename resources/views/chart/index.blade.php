7ed2dddb23d7c37e23aa27a7a77492ca2338d9f2
Jesus Adrian Camacho Rocha <jesus.camacho@ykm.com.mx>
2024-06-17 11:47:59 -0600

    ultimos cambios que no se que som

--- resources/views/chart/index.blade.php
+++ resources/views/chart/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout title="{{ __('Plan de Producción') }}">
     <div class="container grid px-6 mx-auto">
-        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+        <h2 class="my-6 text-xl uppercase font-semibold text-gray-700 dark:text-gray-200">
             ¡{{ __('Hola') }}, {{ Auth::user()->name }}!
         </h2>
         <div class="grid gap-6 mb-8">
