1f441895c0f66160ff8b4817c145576bf66ad770
Miguel Alcala <miguel.alcala@ykm.com.mx>
2024-12-10 09:02:04 -0600

    cambio a live modelos

--- app/Http/Controllers/Planeacion660bController.php
+++ app/Http/Controllers/Planeacion660bController.php
@@ -338,7 +338,7 @@ $contval=0;
         $indata = YK006::query()->insert($datas);
         $indatasql = LOGSUP::query()->insert($datasql);
         $conn = odbc_connect("Driver={Client Access ODBC Driver (32-bit)};System=192.168.200.7;", "LXSECOFR;", "LXSECOFR;");
-        $query = "CALL LX834OU02.YMP006C";
+        $query = "CALL LX834OU.YMP006C";
         $result = odbc_exec($conn, $query);
         $array = explode(",", $TP);
 
@@ -943,7 +943,7 @@ $contval=0;
         $indatasql = LOGSUP::query()->insert($datasql);
 
         $conn = odbc_connect("Driver={Client Access ODBC Driver (32-bit)};System=192.168.200.7;", "LXSECOFR;", "LXSECOFR;");
-        $query = "CALL LX834OU02.YMP006C";
+        $query = "CALL LX834OU.YMP006C";
 
         $result = odbc_exec($conn, $query);
         $array = explode(",", $TP);
