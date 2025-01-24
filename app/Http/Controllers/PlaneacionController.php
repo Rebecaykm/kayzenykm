dc19a8d141afc369628d1958d0b37364157bb9e2
Miguel Alcala <miguel.alcala@ykm.com.mx>
2025-01-24 10:41:36 -0600

    regreso

--- app/Http/Controllers/PlaneacionController.php
+++ app/Http/Controllers/PlaneacionController.php
@@ -278,8 +278,8 @@ class PlaneacionController extends Controller
                 $fechasql = date('Ymd', strtotime($inp[1]));
 
 
-                    // $ar = ["part_number" => $namenA, "date" => $fechasql];
-                    // array_push($datval, $ar);
+                    $ar = ["part_number" => $namenA, "date" => $fechasql];
+                    array_push($datval, $ar);
 
 
                 $dfa = [
@@ -296,26 +296,26 @@ class PlaneacionController extends Controller
                     'K6FIL1' => '',
                     'K6FIL2' => ''
                 ];
-                // $dfasql = [
-                //     'K6PROD' => $namenA,
-                //     'K6WRKC' => $WCT,
-                //     'K6SDTE' => $fecha,
-                //     'K6EDTE' => $fefin,
-                //     'K6DDTE' => $fechasql,
-                //     'K6DSHT' => $turno,
-                //     'K6PFQY' => $request->$plans,
-                //     'K6CUSR' => 'LXSECOFR',
-                //     'K6CCDT' => $load,
-                //     'K6CCTM' => $horasql,
-                //     'K6FIL1' => '',
-                //     'K6FIL2' => ''
-                // ];
-                // array_push($datasql, $dfasql);
+                $dfasql = [
+                    'K6PROD' => $namenA,
+                    'K6WRKC' => $WCT,
+                    'K6SDTE' => $fecha,
+                    'K6EDTE' => $fefin,
+                    'K6DDTE' => $fechasql,
+                    'K6DSHT' => $turno,
+                    'K6PFQY' => $request->$plans,
+                    'K6CUSR' => 'LXSECOFR',
+                    'K6CCDT' => $load,
+                    'K6CCTM' => $horasql,
+                    'K6FIL1' => '',
+                    'K6FIL2' => ''
+                ];
+                array_push($datasql, $dfasql);
                 array_push($datas, $dfa);
             }
             if ($CONT == 160) {
                 $indata = YK006::query()->insert($datas);
-                // $insql = LOGSUP::query()->insert($datasql);
+                 $insql = LOGSUP::query()->insert($datasql);
                 $datas = [];
                 $datasql = [];
                 $CONT = 0;
@@ -425,7 +425,7 @@ class PlaneacionController extends Controller
             }
             if ($CONT == 160) {
                 $indata = YK006::query()->insert($datas);
-                // $insql = LOGSUP::query()->insert($datasql);
+                $insql = LOGSUP::query()->insert($datasql);
                 $datas = [];
                 $datasql = [];
                 $CONT = 0;
@@ -434,7 +434,7 @@ class PlaneacionController extends Controller
         }
 
         $indata = YK006::query()->insert($datas);
-        // $indatasql = LOGSUP::query()->insert($datasql);
+         $indatasql = LOGSUP::query()->insert($datasql);
 
         $conn = odbc_connect("Driver={Client Access ODBC Driver (32-bit)};System=192.168.200.7;", "LXSECOFR;", "LXSECOFR;");
         $query = "CALL LX834OU.YMP006C";
