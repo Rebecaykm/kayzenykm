71e5b734fa9795e533d44a46790d67c48054c8f7
Miguel Alcala <miguel.alcala@ykm.com.mx>
2025-03-10 10:30:22 -0600

    proto mmvo

--- app/Http/Controllers/PlaneacionoffsetController.php
+++ app/Http/Controllers/PlaneacionoffsetController.php
@@ -317,7 +317,7 @@ class PlaneacionoffsetController extends Controller
         $indata = YK006::query()->insert($datas);
         $indatasql = LOGSUP::query()->insert($datasql);
         $conn = odbc_connect("Driver={Client Access ODBC Driver (32-bit)};System=192.168.200.7;", "LXSECOFR;", "LXSECOFR;");
-        $query = "CALL LX834OU.YMP006C";
+        $query = "CALL LX834OU02.YMP006C";
         $result = odbc_exec($conn, $query);
         $array = explode(",", $TP);
 
@@ -452,8 +452,8 @@ class PlaneacionoffsetController extends Controller
 
 
         $conn = odbc_connect("Driver={Client Access ODBC Driver (32-bit)};System=192.168.200.7;", "LXSECOFR;", "LXSECOFR;");
-        // $query = "CALL LX834OU02.YMP006C";
-       $query = "CALL LX834OU.YMR002C";
+        $query = "CALL LX834OU02.YMP006C";
+    //    $query = "CALL LX834OU.YMR002C";
 
          $result = odbc_exec($conn, $query);
         $array = explode(",", $TP);
